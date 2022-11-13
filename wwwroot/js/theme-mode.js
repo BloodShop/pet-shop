f3c5d8f8841a1f2f8eb9596bdff05b4354413666
BloodShop <koliakovcr7@gmail.com>
2022-11-13 22:00:08 +0200

    !

---
+++ wwwroot/js/theme-mode.js
@@ -0,0 +1,9 @@
+﻿//$('.switch').click(() => {
+//    $([".light [class*='-light']", ".dark [class*='-dark']"]).each((i, ele) => {
+//        $(ele).toggleClass('bg-light bg-dark')
+//        $(ele).toggleClass('text-light text-dark')
+//        $(ele).toggleClass('navbar-light navbar-dark')
+//    })
+//    // toggle body class selector
+//    $('body').toggleClass('light dark')
+//});
